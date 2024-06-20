c1de596055c9ffb83f0ca815652d95f5972368d2
Rikolordo <134929851+Rikolordo@users.noreply.github.com>
2024-06-20 12:29:04 +0300

    1

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+work_houres = 1.5
+cource_name = "Python"
+exercises_time = work_houres / completed_homework
+print('Курс:', cource_name, ',', 'всего задач:', completed_homework, ',', 'среднее время выполнения', exercises_time, 'часа.')
---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Daniil'
+print('Name:', name)
+age = 29
+print('Age:', age)
+age = age + 1
+print('New Age:', age)
+is_student = True
+print(is_student)
